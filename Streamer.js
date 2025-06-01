d5d76c9387ff706903b94b623ca36ef261c8f4ec
Erik Pohl <erik.pohl.444@gmail.com>
2025-05-31 22:18:15 -0400

    added function parameters

--- Streamer.js
+++ Streamer.js
@@ -2,12 +2,27 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 
+/**
+ * Yield a simple error response. 
+ * @param {http.ServerResponse} response The first number.
+ * @param {number} statusCode The error status code.
+ * @param {string} contentType The content type of the response.
+ * @param {string} message The error message to send in the response. 
+ * @returns {void}
+ */
 function errResponse(response, statusCode, contentType, message) {
   response.statusCode = statusCode;
   response.setHeader("Content-Type", contentType);
   response.end(message);
 }
 
+/**
+ * Render a file as a response in a single unstreaming result. 
+ * @param {http.ServerResponse} response The response object.
+ * @param {string} filePath The path to the file to render.
+ * @param {string} contentType The content type of the file.
+ * @returns {void}
+ */
 function renderFile(response, filePath, contentType) {
   fs.readFile(filePath, (err, content) => {
     if (err) {
@@ -20,6 +35,13 @@ function renderFile(response, filePath, contentType) {
   });
 }
 
+/**
+ * Render a file as a response in a stream. 
+ * @param {http.ServerResponse} response The response object.
+ * @param {string} filePath The path to the file to stream.
+ * @param {string} contentType The content type of the file.  
+ * @returns {void}
+ */
 function streamFile(response, filePath, contentType) {
   const stream = fs.createReadStream(filePath);
   response.setHeader("Content-Type", contentType);
@@ -29,6 +51,13 @@ function streamFile(response, filePath, contentType) {
   });
 }
 
+/**
+ * Generic handler to serve files based on the style (static or stream). 
+ * @param {http.ServerResponse} response The response object.
+ * @param {string} fileSourceName The name of the file to serve.
+ * @param {string} style The style of serving the file, either "static" or "stream". 
+ * @returns {void}
+ */
 function genericHandler(response, fileSourceName, style) {
   const filePath = path.join(__dirname, "public", fileSourceName);
   const contentType = style === "static" ? "text/html" : "text/plain";
@@ -40,6 +69,12 @@ function genericHandler(response, fileSourceName, style) {
   }
 }
 
+/**
+ * Router function to handle incoming requests and serve files.
+ * @param {http.ServerResponse} response The response object.
+ * @param {http.IncomingMessage} request The incoming request object. 
+ * @returns {void}
+ */
 function router(request, response) {
   if (request.url === "/") {
     response.statusCode = 302;
@@ -63,6 +98,7 @@ exports.htmlHandler = genericHandler;
 const server = http.createServer(router);
 exports.server = server;
 
+// If this module is run directly, start the server.  
 if (require.main === module) {
   server.listen(3000, () => {
     console.log("Server listening on port 3000");
